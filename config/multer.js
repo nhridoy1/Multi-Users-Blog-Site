f7817671400a76344f7fae23c009a1b7906cbe3c
NH Ridoy <ridoydeveloper@gmail.com>
2022-10-16 22:35:12 +0600

    file type validation and file size implemented

--- config/multer.js
+++ config/multer.js
@@ -2,12 +2,11 @@ const multer = require('multer')
 const path = require('path')
 
 const multerConfig = {
-    limit: multer({
-        limits: 1000000
-    }),
-    
+    limits: {
+        // Here we have to give size in bytes 
+        fileSize: 1000000
+    },
     storage: multer.diskStorage({
-        
         destination: function (req, file, cb) {
             cb(null, '/tmp/')
         },
@@ -15,14 +14,22 @@ const multerConfig = {
         filename: function(req, file, cb) {
             const fileExt = path.extname(file.originalname)
             
-            // const filename = file.originalname.replace(/\.[^/.]+$/, "").split(' ').join('-') + '-' + new Date().getTime()
+            // anoter way to do -> file.originalname.replace(/\.[^/.]+$/, "").split(' ').join('-') + '-' + new Date().getTime()
             const filename = path.parse(file.originalname).name.replace(' ', '-') + '-' + new Date().getTime()
 
             cb(null, filename + fileExt)
         }
-    })
+    }),
+    fileFilter: function(req, file, cb) {
+        // another way to do this is -> file.mimetype.startsWith('image') 
+        if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
+            return cb(null, true)
+        } else {
+            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'))
+        }
+    }
 }
 
-const upload = multer({ storage: multerConfig.storage, limits: multerConfig.limit})
+const upload = multer({ storage: multerConfig.storage, limits: multerConfig.limits, fileFilter: multerConfig.fileFilter})
 
 module.exports = upload
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,10 @@
 const User = require('../models/user')
-const storage = require('../config/firebase')
-const { ref, uploadBytesResumable, getDownloadURL } = require('firebase/storage')
 const {BigPromise} = require('../utils/bigPromise')
 const logger = require('../config/logger')
 
 
 exports.signup = BigPromise( async (req, res, next) => {
-    const {name, email, password, phoneNumber} = req.body
-
-    console.log(req.file)
-
+   
+    
     res.status(200).send('bye')
 })
